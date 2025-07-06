58273108938f1e135359844ae6d4367f9bd67af5
PENEKhun <penekhun@gmail.com>
2025-07-06 20:44:52 +0900

    First commit

---
+++ src/main/java/kr/huni/signup/RabbitmqExamplesApplication.java
@@ -0,0 +1,13 @@
+package kr.huni.signup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqExamplesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/huni/signup/RabbitmqExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.huni.signup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqExamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
